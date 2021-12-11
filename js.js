69e984debd8989ca2cb87112a9d946c1e385dadf
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-11 20:32:40 +0000

    changed unmutable lets to consts

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
-let message = document.querySelector("#message");
-let player1Scoreboard = document.querySelector("#player1Scoreboard");
-let player2Scoreboard = document.querySelector("#player2Scoreboard");
-let player1Dice = document.querySelector("#player1Dice");
-let player2Dice = document.querySelector("#player2Dice");
+const message = document.querySelector("#message");
+const player1Scoreboard = document.querySelector("#player1Scoreboard");
+const player2Scoreboard = document.querySelector("#player2Scoreboard");
+const player1Dice = document.querySelector("#player1Dice");
+const player2Dice = document.querySelector("#player2Dice");
 const rollBtn = document.querySelector("#rollBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
