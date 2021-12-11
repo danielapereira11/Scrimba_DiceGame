fff4dcf9a0509e5631e539cc0a432c2083f3faa0
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-11 21:34:48 +0000

    Set generateRaondomNumber function upon click of "roll dice" btn

--- js.js
+++ js.js
@@ -9,3 +9,25 @@ const resetBtn = document.querySelector("#resetBtn");
 let player1Score = 0;
 let player2Score = 0;
 let player1Turn = true;
+function showDisplayBtn() {
+  player1Dice.classList.remove("active");
+  player2Dice.classList.add("active");
+}
+function generateRandomNumber() {
+  if (player1Turn === true) {
+    player1RandomNumber = Math.ceil(Math.random() * 6);
+    player1Dice.innerHTML = player1RandomNumber;
+    player1Score += player1RandomNumber;
+    player1Scoreboard.innerHTML = player1Score;
+    message.innerHTML = "Player 2 Turn";
+    showDisplayBtn();
+  } else {
+    player2RandomNumber = Math.ceil(Math.random() * 6);
+    player2Dice.innerHTML = player2RandomNumber;
+    player2Score += player2RandomNumber;
+    player2Scoreboard.innerHTML = player2Score;
+    message.innerHTML = "Player 1 Turn";
+    showDisplayBtn();
+  }
+}
+rollBtn.addEventListener("click", generateRandomNumber);
