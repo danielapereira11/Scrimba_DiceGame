27e0e2c047c6932f044d41f1fce5b8e0d1ddcf05
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-12-11 20:30:17 +0000

    Variable creation

---
+++ js.js
@@ -0,0 +1,11 @@
+let message = document.querySelector("#message");
+let player1Scoreboard = document.querySelector("#player1Scoreboard");
+let player2Scoreboard = document.querySelector("#player2Scoreboard");
+let player1Dice = document.querySelector("#player1Dice");
+let player2Dice = document.querySelector("#player2Dice");
+const rollBtn = document.querySelector("#rollBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
